931c34882e8641b8856fb1d4f47fde21df256364
Oberfeldwedler <samuelbrunner8@gmail.com>
2024-03-16 00:59:36 +0100

    started implementing mysqlCaller

--- main.py
+++ main.py
@@ -1,47 +1,57 @@
 import PySimpleGUI as sg
 import mysql.connector
+from mysql.connector import errorcode
 import configparser
+
 
 sg.theme('BluePurple')
 font = ("Arial", 15)
 initialWidth=951
 initialHeight=540
 
-
 header = [[ 
     sg.Text('Bitte Ausweis scannen') 
 ]]
-
 product0 = [[
     sg.Column( [[ sg.Text('Getränk') ]] ), 
     sg.Push(),
     sg.Column( [[ sg.Text('15€') ]] ), 
     sg.Column( [[ sg.Button('X', size=5) ]] )
 ]] 
-
 sum = [[ 
     sg.Column( [[sg.Text('Summe')]] ), 
     sg.Push(), 
     sg.Column( [[sg.Text('80€')]] )
 ]]
-
 productList = [
     [ product0 ],
     [ sg.VPush() ], 
     [ sg.HorizontalSeparator() ],
     [ sum ]
 ]
-
 footer = [[ 
     # sg.Button('Reset'), sg.Push() ,sg.Button('Buchen')
     sg.Button( 'Reset', size=20 ), sg.Button('Buchen', expand_x=True ) 
 ]]
-
 layout = [
     [ sg.Frame( 'Fachschaftsmitglied', header , expand_x=True, element_justification='center' ) ],
     [ sg.Frame( 'Einkaufsliste', productList , expand_x=True, expand_y=True ) ],
     [ sg.Frame( '', footer, expand_x=True ) ]
 ]
+
+
+class user:
+    def __init__(self, barcode, mySqlCaller):
+        self.barcode = barcode
+        self.name, self.current_balance =  mySqlCaller.getUserFromBarcode()
+
+
+class product:
+    def __init__(self, barcode, mySqlCaller):
+        self.barcode = barcode
+        self.name, self.price = mySqlCaller.getProductFromBarcode()
+   
+
 
 class mySqlCaller_Exception(Exception):
     """Raised when an error occurred during the communication with MySQL Server"""
@@ -54,17 +64,40 @@ class mySqlCaller:
         self.password = mySqlSettingsDict["password"]
         self.database = mySqlSettingsDict["database"]
 
-    def establishConnectionToServer(self):
+
+    def establishConnection(self):
         try:
-            cnx = mysql.connector.connect(user=self.username, password=self.password,
+            self.cnx = mysql.connector.connect(user=self.username, password=self.password,
                                     host=self.hostAddress, port=self.portNumber,
                                     database=self.database)
         except mysql.connector.Error as err:
-            raise mySqlCaller_Exception(
-                f"ERROR: Could not establish a Database connection to {self.hostAddress}:{self.portNumber}")
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+
+    def createDictCursor(self):
+        self.dictCursor = cnx.cursor()
         
-    # def closeConnectionToServer(self):
-    #     cnx.cursor()
+    def closeConnection(self):
+        self.cnx.close()
+       
+    def getUserInfoFromBarcode(self, barcode):
+        getUser = ("SELECT * FROM employees WHERE barcode=%s")
+        try:
+            self.dictCursor.execute(getUser, barcode)
+            if self.dictCursor.rowcount > 1: 
+                raise mySqlCaller_Exception
+        # except mysql.connector.Error as err:
+        #     print("Syntax error: {}".format(err))
+        except mySqlCaller_Exception:
+            print ("User barcode is not unique in Database.")
+            return None
+        return dictCursor.fetchone()
+
+
 
 
 # Create the Window
@@ -78,10 +111,16 @@ window = sg.Window (
     font=font
 )
 window.Resizable=True
+
+
+
 config = configparser.ConfigParser()
 config.read('km3003.conf')
 mySqlSettingsDict = dict(config['mysql'])
 
+mySqlCaller = mySqlCaller(mySqlSettingsDict)
+mySqlCaller.establishConnection()
+mySqlCaller.createDictCursor()
 
 
 # Event Loop to process "events" and get the "values" of the inputs
