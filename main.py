cc7f0486cd9a2b97d30e1726170768b8445e30c2
Samuel Brunner <samuelbrunner8@gmail.com>
2026-03-06 22:58:43 +0100

    Revert "wake screen on scan"
    This reverts commit 9d44a51520e4ba1156858604489f192e34709589.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 import time
 import logging
 import asyncio
-import pyautogui
 import configparser
 import logging.handlers
 from decimal import Decimal
@@ -183,11 +182,6 @@ def onShutdown():
     database_caller.closeConnection()
 app.on_shutdown(onShutdown)
 
-def wakeScreen():
-    # Move mouse 1 pixel and back to trigger 'activity'
-    logger.info('Wake screen by movin the curser 1 pixel.')
-    pyautogui.moveRel(1, 0)
-    pyautogui.moveRel(-1, 0)
 
 # ====================================================================
 # Scanner
@@ -257,7 +251,6 @@ def processBarcode(barcode):
 def scanner_poller():
     logger.info('scanner_poller')
     barcode = scanner_instance.getBarcode()
-    wakeScreen()
     if barcode:
         logger.info(f"New barcode was scanned: {barcode}")
         processBarcode(barcode)
