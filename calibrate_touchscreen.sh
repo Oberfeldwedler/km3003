900dce98c16ed344183d012c496f085a555d1f35
Samuel Brunner <samuelbrunner8@gmail.com>
2026-03-06 22:23:49 +0100

    add calibration script for touchscreen

---
+++ calibrate_touchscreen.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+/usr/bin/xinput set-prop "eGalax Inc. USB TouchController" "libinput Calibration Matrix" 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0
+/usr/bin/xinput set-prop "eGalax Inc. USB TouchController" "libinput Calibration Matrix" -1.1497, 0.0, 1.0737, 0.0, -1.1316, 1.0724, 0.0, 0.0, 1.0
